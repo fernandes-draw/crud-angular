7ecaa45df18c57f1b176f669e78c5a02d0f58ebb
fernandes-draw <fernandes_draw@hotmail.com>
2024-05-21 21:35:59 -0300

    Adicionando spinner e modal de erro

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -8,11 +11,25 @@ import { CoursesService } from '../services/courses.service';
   styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos');
+        return of([]);
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpCliente: HttpClient) {}
 
-  list(): Course[] {
-    return [
-      {
-        _id: '1',
-        name: 'Angular',
-        category: 'Front-End',
-      },
-    ];
+  list() {
+    return this.httpCliente.get<Course[]>(this.API).pipe(
+      first(),
+      delay(2000),
+      tap((courses) => console.log(courses))
+    );
   }
 }
